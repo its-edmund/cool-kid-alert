71e30642204ac71a8c2d312b1bd58c1fe36c2c0b
Edmund Xin <mxinburritos@gmail.com>
2020-09-12 23:45:26 -0700

    retort changed to respond

--- commands/toggle.js
+++ commands/toggle.js
@@ -4,10 +4,10 @@ module.exports = {
   execute(msg, args) {
     if (args.toggled) {
       console.log(args);
-      msg.channel.send('Retorting has now been turned off.');
+      msg.channel.send('Responding has now been turned off.');
       args.toggled = false;
     } else {
-      msg.channel.send('Retorting has now been turned on.');
+      msg.channel.send('Responding has now been turned on.');
       args.toggled = true;
     }
   },
