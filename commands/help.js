7076eb5f7469f4d4a6deb06a5f40ee511a4527d7
Edmund Xin <mxinburritos@gmail.com>
2020-09-14 00:09:26 -0700

    lol

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ module.exports = {
   description: 'list purpose and commands',
   execute(msg, args) {
     msg.channel.send(
-      "I'm a bot created to help deter people from swearing too much in Discord"
+      "I've been created to anally reck @Nathan. He will destroyed."
     );
   },
 };
