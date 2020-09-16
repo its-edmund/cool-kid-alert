ee928e660b899d719badff198250030ae37fbad6
Edmund Xin <mxinburritos@gmail.com>
2020-09-15 21:57:33 -0700

    help message fixed

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ module.exports = {
   description: 'list purpose and commands',
   execute(msg, args) {
     msg.channel.send(
-      "I've been created to anally reck @Nathan. He will destroyed."
+      "I've been created to anally reck @Nathan. He will be destroyed."
     );
   },
 };
